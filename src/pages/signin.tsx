78a3a792c2c383f1470f604d3ad235b056715b6d
William Golden <wtgolden5@gmail.com>
2023-12-05 13:55:25 -0500

    signin page functional

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,22 +1,109 @@
 import { signIn } from "next-auth/react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+interface IFormInput {
+  email: string;
+  password: string;
+}
 
 const SignIn = () => {
-  const click = async () => {
-    signIn("credentials", {
-      email: "wtgolden5@gmail.com",
-      password: "",
-      callbackUrl: "http://localhost:3000",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const { email, password } = data;
+    await signIn("credentials", {
+      email,
+      password,
+      callbackUrl: `/`,
     });
   };
+
   return (
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        click();
-      }}
-    >
-      Login
-    </button>
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Blackbox Login
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Welcome to the Blackbox. Please login to continue.
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email"
+                {...register("email", { required: "Email is required" })}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+                {...register("password", { required: "Password is required" })}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <button
+            type="button"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-300 px-4 py-2 text-sm font-medium text-black hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2"
+          >
+            Login with Alpaca
+          </button>
+        </div>
+        <div className="mt-6 text-center">
+          <a
+            href="#"
+            className="font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            Don't have an account? Sign up
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
