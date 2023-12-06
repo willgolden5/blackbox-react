c95f35e19d7cfb79a5dade7d993cf9cb7f5ff1fe
William Golden <wtgolden5@gmail.com>
2023-12-05 19:17:28 -0500

    color

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -40,7 +40,7 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8">
+    <div className="flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
           <h2 className="mt-6 text-center text-4xl font-bold text-black">
